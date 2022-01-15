4aeea68ac1989c71ad2ed1a222f8a1f3bb2ce50b
earthPerson-001 <neupanebishal2001@gmail.com>
2022-01-15 19:55:23 +0545

    added Cmake file and compiling executables for windows and linux

---
+++ compile.bat
@@ -0,0 +1,19 @@
+@echo off
+TITLE "Fandral Editor"
+
+::Uncomment the below two lines if running for the first time replace MinGW Makefiles to whichever build generator you're using
+
+::set makefile-type="MinGW Makefiles"
+::cmake -B ./build -S . -G %makefile-type%
+
+
+::changing to build directory
+cd build
+
+::running cmake command there
+cmake ../
+
+::making the executables
+cmake --build . && Editor
+
+cd ..
---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#Uncomment the below two lines if running for the first time and replace MinGW Makefiles to whichever build generator you're using
+
+#MAKEFILE_TYPE="MinGW Makefiles"
+#cmake -B ./build -S . -G "$MAKEFILE_TYPE"
+
+cd build
+
+cmake ../
+
+cmake --build .
+
+if [ $? ] 
+then 
+    ./Editor
+else
+    echo "Error occured during compilation. \n"
+fi
+
+cd ..
