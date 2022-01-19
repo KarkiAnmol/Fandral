63442d46b246aa0f6b2e43d057fa5883298832b9
earthPerson-001 <neupanebishal2001@gmail.com>
2022-01-19 10:26:57 +0545

    cleaned few imports and separated from main window. Only OnInit() function will address the MyFrame class

---
+++ include/app.h
@@ -0,0 +1,40 @@
+/**Header file for app.cpp **/
+
+// Define a new application type, each program should derive a class from wxApp
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWidgets headers)
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+// ----------------------------------------------------------------------------
+// resources
+// ----------------------------------------------------------------------------
+
+// the application icon (under Windows and OS/2 it is in resources and even
+// though we could still include the XPM here it would be unused)
+#ifndef wxHAS_IMAGES_IN_RESOURCES
+    #include "../sample.xpm"
+#endif
+
+#include <AppConfig.h>
+
+class App : public wxApp
+{
+public:
+    // override base class virtuals
+    // ----------------------------
+
+    // this one is called on application startup and is a good place for the app
+    // initialization (doing it here and not in the ctor allows to have an error
+    // return: if OnInit() returns false, the application terminates)
+    virtual bool OnInit();
+};
