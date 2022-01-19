1eabcb6f81ae2a2841e87575168d09e6ed308b46
earthPerson-001 <neupanebishal2001@gmail.com>
2022-01-19 21:00:44 +0545

    Added edit menu(haven't connected with the events yet). Added proper event handler for save, save as and open events.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,22 +1,24 @@
-#include <mainwindow.h>
+#include "mainwindow.h"
 
 // ----------------------------------------------------------------------------
 // main frame
 // ----------------------------------------------------------------------------
 
 // frame constructor
-MyFrame::MyFrame(const wxString& title)
-       : wxFrame(NULL, Main_Window, title, wxPoint(POS_X, POS_Y), wxSize(WIDTH, HEIGHT))
+MyFrame::MyFrame(const wxString &title)
+    : wxFrame(NULL, Main_Window, title, wxPoint(MAIN_WINDOW_POS_X, MAIN_WINDOW_POS_Y), wxSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT))
 {
     // set the frame icon
     SetIcon(wxICON(sample));
 
 #if wxUSE_MENUS
 
-    mainTextBox = new wxTextCtrl(this, TEXT_Main, _("Hi!"), wxDefaultPosition, wxDefaultSize,  
-    wxTE_MULTILINE | wxTE_RICH , wxDefaultValidator, wxTextCtrlNameStr);
+    mainTextBox = new wxTextCtrl(this, TEXT_Main, "", wxDefaultPosition, wxDefaultSize,
+                                 wxTE_MULTILINE | wxTE_RICH, wxDefaultValidator, wxTextCtrlNameStr);
+    mainTextBox->SetBackgroundColour(wxColour(8, 0, 23, 0.77));
+    mainTextBox->SetForegroundColour(wxColour(134, 188, 123, 0.77));
 
-    // create a menu bar
+    // create file menu
     wxMenu *fileMenu = new wxMenu;
     fileMenu->Append(New_File, "&New\tCtrl-N", "Open new text file");
     fileMenu->Append(Open_File, "&Open", "Open saved text files");
@@ -24,13 +26,19 @@ MyFrame::MyFrame(const wxString& title)
     fileMenu->Append(Save_File_As, "Save As", "Save the file as");
     fileMenu->Append(Editor_Quit, "E&xit\tAlt-X", "Quit this program");
 
-    // the "About" item should be in the help menu
+    // create edit menu
+    wxMenu *editMenu = new wxMenu;
+    editMenu->Append(Find_Item, "&Find\tCtrl-F", "Find Items");
+    editMenu->Append(Find_And_Replace_Item, "Find And &Replace \tCtrl-R", "Find Items and Replace");
+
+    // create help menu
     wxMenu *helpMenu = new wxMenu;
     helpMenu->Append(Editor_About, "&About\tF1", "Show about dialog");
 
     // now append the freshly created menu to the menu bar...
     wxMenuBar *menuBar = new wxMenuBar();
     menuBar->Append(fileMenu, "&File");
+    menuBar->Append(editMenu, "&Edit");
     menuBar->Append(helpMenu, "&Help");
 
     // ... and attach this menu bar to the frame
@@ -42,7 +50,7 @@ MyFrame::MyFrame(const wxString& title)
     CreateStatusBar(1);
     //SetStatusText (const wxString &text, int i=0)
     int statusBarFistPart = 0; // statusBarSecondPart = 1, statusBarThirdPart = 2;
-    SetStatusText("Here goes nothing", statusBarFistPart);
+    SetStatusText("Welcome! to Fandral Text Editor", statusBarFistPart);
     //SetStatusText("Don't compare with VScode", statusBarThirdPart);
 
 #endif // wxUSE_STATUSBAR
@@ -56,47 +64,101 @@ MyFrame::MyFrame(const wxString& title)
 // handlers) which process them. It can be also done at run-time, but for the
 // simple menu events like this the static method is much simpler.
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-    EVT_MENU(Editor_Quit,  MyFrame::OnQuit)
+    EVT_MENU(Editor_Quit, MyFrame::OnQuit)
     EVT_MENU(Editor_About, MyFrame::OnAbout)
     EVT_MENU(New_File, MyFrame::OnNew)
     EVT_MENU(Save_File, MyFrame::OnSave)
+    EVT_MENU(Open_File, MyFrame::OnOpen)
+    EVT_MENU(Save_File_As, MyFrame::OnSaveAs)
 wxEND_EVENT_TABLE()
 
-
+    
 // event handlers
 
-void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
+void MyFrame::OnQuit(wxCommandEvent &WXUNUSED(event))
 {
     // true is to force the frame to close
     Close(true);
 }
 
-void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+void MyFrame::OnAbout(wxCommandEvent &WXUNUSED(event))
 {
-    wxMessageBox(wxString::Format
-                 (
-                    "Welcome to Fandral Editor made with %s!\n"
-                    "\n"
-                    "This is the initial build of Fandral Editor\n"
-                    "We will be adding other features soon."
-                    "\nYour os Description is %s.",
-                    wxVERSION_STRING,
-                    wxGetOsDescription()
-                 ),
+    wxMessageBox(wxString::Format(
+                     "Welcome to Fandral Editor made with %s!\n"
+                     "\n"
+                     "This is the initial build of Fandral Editor\n"
+                     "We will be adding other features soon."
+                     "\nYour os Description is %s.",
+                     wxVERSION_STRING,
+                     wxGetOsDescription()),
                  "About Fandral Editor",
                  wxOK | wxICON_INFORMATION,
                  this);
 }
 
-void MyFrame::OnSave(wxCommandEvent& WXUNUSED(event)){
+void MyFrame::OnOpen(wxCommandEvent &WXUNUSED(event))
+{
+    //Wildcard example for file selection
+    //"BMP and GIF files (*.bmp;*.gif)|*.bmp;*.gif|PNG files (*.png)|*.png"
+
+    wxString openLocation = wxLoadFileSelector(
+        "a text",
+        "txt files (*.txt)|*.txt| DOCX files (*.docx)|*.docx| XML files (*.xml)|*.xml",
+        "Fandral",
+        this);
+
+    bool append = true;
+
+    int index = openedFiles->Index(openLocation);
+
+    if (index == wxNOT_FOUND)
+    {
+        openedFiles->Add(openLocation);
+        index = openedFiles->Index(openLocation);
+    }
+
+    std::cout << openLocation;
+
+    currentlyOpenFileIndex = index;
     
+    mainTextBox->LoadFile(openLocation);
 }
 
-void MyFrame::OnNew(wxCommandEvent& WXUNUSED(event))
+void MyFrame::OnSave(wxCommandEvent &WXUNUSED(event))
+{
+    wxString saveLocation;
+    if(currentlyOpenFileIndex < 0)
+    {
+        saveLocation = wxSaveFileSelector(
+            "the current text",
+            "TEXT files (*.txt)|*.txt| DOCX files (*.docx)|*.docx| XML files (*.xml)|*.xml",
+            "Fandral",
+            this);
+    }
+    else{
+        saveLocation = openedFiles->Item(currentlyOpenFileIndex);
+    }
+
+    mainTextBox->SaveFile(saveLocation);
+}
+
+
+void MyFrame::OnSaveAs(wxCommandEvent &WXUNUSED(event))
+{
+    wxString saveLocation = wxSaveFileSelector(
+            "the current text",
+            "TEXT files (*.txt)|*.txt| DOCX files (*.docx)|*.docx| XML files (*.xml)|*.xml",
+            "Fandral",
+            this);
+
+    mainTextBox->SaveFile(saveLocation);
+}
+
+
+void MyFrame::OnNew(wxCommandEvent &WXUNUSED(event))
 {
     MyFrame *secondaryWindow = new MyFrame(wxString::Format("New Window"));
 
-   
     Close(true);
 
     secondaryWindow->Show(true);
