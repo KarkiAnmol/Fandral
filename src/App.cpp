0e048a83952383f1457ae05809bae84280a6d4ed
earthPerson-001 <neupanebishal2001@gmail.com>
2022-01-20 22:44:10 +0545

    Added placeholder for splash image

--- src/App.cpp
+++ src/App.cpp
@@ -1,6 +1,7 @@
 
 #include "app.h"
-#include "mainwindow.h"
+#include "mainwindow.h"     //For MyFrame class
+#include <wx/splash.h>      //For wxSplashScreen
 
 // ============================================================================
 // implementation
@@ -22,9 +23,20 @@ bool App::OnInit()
     title = wxString::Format("Fandral Editor  V %d.%d    ",
         (int)Editor_VERSION_MAJOR,
         (int)Editor_VERSION_MINOR);
-    
+
     // create the main application window
     MyFrame *frame = new MyFrame(title);
+
+    //For splash screen on startup
+    wxBitmap bitmap;
+    if (bitmap.LoadFile("../resources/fandralSplash.png", wxBITMAP_TYPE_PNG))
+    {
+        wxSplashScreen* splash = new wxSplashScreen(bitmap,
+            wxSPLASH_CENTRE_ON_PARENT|wxSPLASH_TIMEOUT,
+            1500, frame, -1, wxPoint(MAIN_WINDOW_POS_X, MAIN_WINDOW_POS_Y), wxSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT),
+            wxBORDER_SIMPLE|wxSTAY_ON_TOP);
+    }
+    wxYield();
 
     // show the application window (the frames, unlike simple controls, are not shown when
     // created initially)
