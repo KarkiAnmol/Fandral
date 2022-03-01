c4c0dc5f20b09e247f0e01d1ae2119c3622ba02b
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-03-01 18:42:38 +0545

    Added handlers for methods for save and saveAs Changed parent from wxwindow to wxnotebook which makes it easier to update tab label

--- src/TextCtrl.cpp
+++ src/TextCtrl.cpp
@@ -4,23 +4,24 @@
 
 #include "textctrl.hpp"
 
-TextCtrl::TextCtrl(wxWindow *parentWindow, wxWindowID wx_ID, const wxString name)
-    : wxStyledTextCtrl(parentWindow, wx_ID, wxDefaultPosition, wxDefaultSize,
-                       wxTE_MULTILINE | wxTE_RICH, name)
+#include "tab.hpp"
+
+TextCtrl::TextCtrl(wxNotebook* parentNotebook, wxWindowID wx_ID, const wxString name)
+    : wxStyledTextCtrl(parentNotebook, wx_ID, wxDefaultPosition, wxDefaultSize,
+                       wxTE_MULTILINE | wxTE_RICH, name), parentNotebook(parentNotebook)
 {
 
-    //dynamically binding event handlers
+    // dynamically binding event handlers
     Bind(wxEVT_KEY_DOWN, &TextCtrl::KeyEvent, this);
 
-
-    //Initially setting it to be editable
+    // Initially setting it to be editable
     this->SetEditable(true);
 
-    //setting the initial styles
+    // setting the initial styles
 
     this->StyleClearAll();
 
-    //line numbers
+    // line numbers
 
     this->SetMarginWidth(MARGIN_LINE_NUMBERS, 25);
 
@@ -30,11 +31,15 @@ TextCtrl::TextCtrl(wxWindow *parentWindow, wxWindowID wx_ID, const wxString name
     this->SetMarginType(MARGIN_LINE_NUMBERS, wxSTC_MARGIN_NUMBER);
 }
 
-//Event handlers
+TextCtrl::TextCtrl(wxNotebook* parentNotebook, wxWindowID wx_ID, const wxString filePath, const wxString name)
+    : TextCtrl(parentNotebook, wx_ID, name)
+{
+    this->filePath = filePath;
+}
 
 void TextCtrl::KeyEvent(wxKeyEvent &event)
 {
-    
+
     wxChar uc = event.GetUnicodeKey();
     if (uc != WXK_NONE)
     {
@@ -43,53 +48,56 @@ void TextCtrl::KeyEvent(wxKeyEvent &event)
         // WXK_BACK, so check for them explicitly.
         if (uc >= 32)
         {
-            if(!this->IsEditable()) //only do this if the editor is in command mode
+            if (!this->IsEditable()) // only do this if the editor is in command mode
             {
-                switch(uc)
+                switch (uc)
                 {
-                    //If i is pressed when the text ctrl is not editable, set it back to be editable
-                    //this is only for now, the implementation might be different in future
-                    case 73:  //B --> 73
-                        this->SetEditable(true);
-                        break;
+                // If i is pressed when the text ctrl is not editable, set it back to be editable
+                // this is only for now, the implementation might be different in future
+                case 73: // B --> 73
+                    this->SetEditable(true);
+                    break;
 
-                    //move cursor left
-                    case 72: //H--> 72
-                        this->CharLeft();
-                        break;
+                // move cursor left
+                case 72: // H--> 72
+                    this->CharLeft();
+                    break;
 
-                    //move cursor down 
-                    case 74: //J--> 74
-                        this->LineDown();
-                        break;
+                // move cursor down
+                case 74: // J--> 74
+                    this->LineDown();
+                    break;
 
-                    //move cursor up
-                    case 75: //K--> 75
-                        this->LineUp();
-                        break;
+                // move cursor up
+                case 75: // K--> 75
+                    this->LineUp();
+                    break;
 
-                    //move cursor right
-                    case 76: //L--> 76
-                        this->CharRight();
-                        break;
-                    
-                    default:
-                        event.Skip();
+                // move cursor right
+                case 76: // L--> 76
+                    this->CharRight();
+                    break;
+
+                default:
+                    event.Skip();
                 }
             }
-            else event.Skip();
+            else
+                event.Skip();
         }
         else
         {
             // It's a control character
-            switch(uc)
+            switch (uc)
             {
-                // If escape key is pressed, enter into command mode
-                case WXK_ESCAPE:
-                    this->SetEditable(false);
-                    break;
-                default:
-                    event.Skip();
+            // If escape key is pressed, enter into command mode
+            case WXK_ESCAPE:
+                this->SetEditable(false);
+                break;
+            case WXK_CONTROL_S:
+                this->_SaveFile();
+            default:
+                event.Skip();
             }
         }
     }
@@ -99,3 +107,73 @@ void TextCtrl::KeyEvent(wxKeyEvent &event)
     }
     return;
 };
+
+void TextCtrl::_SaveFile()
+{
+    wxString saveLocation;
+
+    /**Opens native file explorer dialog box to select saving location
+     * if the file isn't saved previously or new file is open
+     **/
+    if (this->filePath.Cmp(_T("")) == 0)
+    {
+        this->_SaveFileAs();
+        return;
+    }
+    else
+    {
+        // sets saveLocation to currently open file save location if it's known.
+        saveLocation = this->filePath;
+    }
+    
+    this->SaveFile(saveLocation);
+    this->updateNameLabel(saveLocation);
+}
+
+void TextCtrl::_SaveFileAs()
+{
+    // Opens native file explorer dialog box to select saving location
+    wxString saveLocation = wxSaveFileSelector(
+        "the current text",
+        "TEXT files (*.txt)|*.txt| DOCX files (*.docx)|*.docx| XML files (*.xml)|*.xml",
+        "Fandral",
+        this);
+
+    // Modifying the filepath of the tab object
+    this->filePath = saveLocation;
+
+    this->SaveFile(saveLocation);
+    this->updateNameLabel(saveLocation);
+}
+
+wxString TextCtrl::updateNameLabel(const wxString &fileLocation)
+{
+    this->updateTabFilePaths();
+    wxString tempFileLocation = fileLocation.Clone();
+
+// The file separator in windows will be '\\' so it is replaced with '\'
+#if defined __WXMSW__
+    // this will be file name only
+    tempFileLocation.Replace("\\", "/", true);
+#endif
+
+    wxString fileNameWithExtension = tempFileLocation.AfterLast(_T('/'));
+
+    this->parentNotebook->SetPageText(this->parentNotebook->GetSelection(), fileNameWithExtension);
+
+    return fileNameWithExtension;
+}
+
+bool TextCtrl::updateTabFilePaths()
+{
+    for(Tab& t: Tab::getActiveTabsVector())
+    {
+        if(t.getCurrentlyActiveTextBox() == *this)
+        {
+            t.setFilePath(this->filePath);
+            break;
+        }
+    }
+
+    return 1;
+}
