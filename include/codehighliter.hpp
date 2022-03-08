8a3ebf3b9f027970a9bb191a46e135402f08ec78
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-03-08 20:45:55 +0545

    Separated syntax highliting into different class

---
+++ include/codehighliter.hpp
@@ -0,0 +1,64 @@
+#if defined _CODE_HIGHLITER_H_==0
+#define _CODE_HIGHLITER_H_ 1
+
+#include "wx/stc/stc.h"
+
+#define CODEHIGHLITER_ALICE_BLUE wxColour("#F0F8FF")
+#define CODEHIGHLITER_ANTIQUE_WHITE wxColour("#FAEBD7")
+#define CODEHIGHLITER_AQUA wxColour("#00FFFF")
+#define CODEHIGHLITER_AQUAMARINE wxColour("#7FFFD4")
+#define CODEHIGHLITER_AZURE wxColour("#F0FFFF")
+#define CODEHIGHLITER_BEIGE wxColour("#F5F5DC")
+#define CODEHIGHLITER_BISQUE wxColour("#FFE4C4")
+#define CODEHIGHLITER_BLACK wxColour("#000000")
+#define CODEHIGHLITER_BLANCHED_ALMOND wxColour("#FF3BCD")
+#define CODEHIGHLITER_BLUE wxColour("#0000FF")
+#define CODEHIGHLITER_BLUE_VIOLET wxColour("#8A2BE2")
+#define CODEHIGHLITER_BROWN wxColour("#A52A2A")
+#define CODEHIGHLITER_BLURY_WOOD wxColour("#DEB887")
+#define CODEHIGHLITER_CADET_BLUE wxColour("#5F9EA0")
+#define CODEHIGHLITER_CHARTREUSE wxColour("#7FFF00")
+#define CODEHIGHLITER_CHOCOLATE wxColour("#D2691E")
+#define CODEHIGHLITER_CORAL wxColour("#FF7F50")
+#define CODEHIGHLITER_CORN_FLOWER_BLUE wxColour("#6495ED")
+#define CODEHIGHLITER_CORN_SILK wxColour("#FFF8DC")
+#define CODEHIGHLITER_CRIMSON wxColour("#DC143C")
+#define CODEHIGHLITER_CYAN wxColour("#00FFFF")
+#define CODEHIGHLITER_DARK_BLUE wxColour("#00008B")
+#define CODEHIGHLITER_DARK_CYAN wxColour("#008B8B")
+#define CODEHIGHLITER_DEEP_SKY_BLUE wxColour("#00BFFF")
+#define CODEHIGHLITER_NAVY wxColour("#000080")
+#define CODEHIGHLITER_ORCHID wxColour("#DA70D6")
+#define CODEHIGHLITER_RED wxColour("#FF0000")
+#define CODEHIGHLITER_VIOLET wxColour("#EE82EE")
+
+
+
+class CodeHighliter
+{
+public:
+    wxStyledTextCtrl* textctrl;
+
+    int lex_language;
+    
+    CodeHighliter(wxStyledTextCtrl* textctrl);
+
+    CodeHighliter(wxStyledTextCtrl* textctrl, int lex_language);
+
+    void setLex_Language(int lex_language);
+
+    bool highlightCPP();
+
+    bool highlightPython();
+
+    bool highlightJava();
+
+    bool highlightC();
+
+    bool highlightJavaScript();
+
+    bool highlightCSharp();
+
+};
+
+#endif // _CODE_HIGHLITER_H_
---
+++ src/CodeHighliter.cpp
@@ -0,0 +1,81 @@
+#include "CodeHighliter.hpp"
+
+CodeHighliter::CodeHighliter(wxStyledTextCtrl* textctrl): textctrl(textctrl){};
+
+CodeHighliter::CodeHighliter(wxStyledTextCtrl* textctrl, int lex_language): textctrl(textctrl)
+{
+    this->setLex_Language(lex_language);
+}
+
+void CodeHighliter::setLex_Language(int lex_language)
+{
+    this->lex_language = lex_language;
+
+    switch(lex_language)
+    {
+        case(wxSTC_LEX_CPP):
+            this->highlightCPP();
+            break;
+        case(wxSTC_LEX_PYTHON):
+            this->highlightPython();
+            break;
+    }
+}
+
+bool CodeHighliter::highlightCPP()
+{
+    this->lex_language=wxSTC_LEX_CPP;
+
+    this->textctrl->SetLexer(wxSTC_LEX_CPP);
+
+    this->textctrl->StyleSetForeground(wxSTC_C_DEFAULT, this->textctrl->GetDefaultStyle().GetTextColour());
+    this->textctrl->StyleSetForeground (wxSTC_C_STRING,            CODEHIGHLITER_CHOCOLATE);
+    this->textctrl->StyleSetForeground (wxSTC_C_PREPROCESSOR,      CODEHIGHLITER_CRIMSON);
+    this->textctrl->StyleSetForeground (wxSTC_C_IDENTIFIER,        CODEHIGHLITER_ANTIQUE_WHITE);
+    this->textctrl->StyleSetForeground (wxSTC_C_NUMBER,            CODEHIGHLITER_CYAN);
+    this->textctrl->StyleSetForeground (wxSTC_C_CHARACTER,         CODEHIGHLITER_BEIGE);
+    this->textctrl->StyleSetForeground (wxSTC_C_WORD,              CODEHIGHLITER_VIOLET);
+    this->textctrl->StyleSetForeground (wxSTC_C_OPERATOR,          wxColor("WHITE"));
+    this->textctrl->StyleSetForeground (wxSTC_C_WORD2,             CODEHIGHLITER_CADET_BLUE);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENT,           CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTLINE,       CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOC,        CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOCKEYWORD, CODEHIGHLITER_BLUE_VIOLET);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOCKEYWORDERROR, CODEHIGHLITER_RED);
+    this->textctrl->StyleSetForeground (wxSTC_C_USERLITERAL,       CODEHIGHLITER_DARK_BLUE);
+
+    // sample list of keywords, I haven't included them all to keep it short...
+    this->textctrl->SetKeyWords(0, wxT("return for while break continue"));
+    this->textctrl->SetKeyWords(1, wxT("const int float void char double"));
+    this->textctrl->SetKeyWords(2, wxT("class struct std"));
+
+    return true;
+}
+
+bool CodeHighliter::highlightPython()
+{
+    this->lex_language=wxSTC_LEX_PYTHON;
+
+    this->textctrl->SetLexer(wxSTC_LEX_PYTHON);
+
+}
+
+bool CodeHighliter::highlightJava()
+{
+
+}
+
+bool CodeHighliter::highlightC()
+{
+
+}
+
+bool CodeHighliter::highlightJavaScript()
+{
+
+}
+
+bool CodeHighliter::highlightCSharp()
+{
+
+}
