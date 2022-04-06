dc44bc4d3e4455d5b9bc21bcbe2b7536530c9bd6
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2022-04-06 18:45:05 +0545

    Managed indentation and changed the return type to void

--- src/CodeHighliter.cpp
+++ src/CodeHighliter.cpp
@@ -22,37 +22,36 @@ void CodeHighliter::setLex_Language(int lex_language)
     }
 }
 
-bool CodeHighliter::highlightCPP()
+void CodeHighliter::highlightCPP()
 {
     this->lex_language=wxSTC_LEX_CPP;
 
     this->textctrl->SetLexer(wxSTC_LEX_CPP);
 
-    this->textctrl->StyleSetForeground(wxSTC_C_DEFAULT, this->textctrl->GetDefaultStyle().GetTextColour());
-    this->textctrl->StyleSetForeground (wxSTC_C_STRING,            CODEHIGHLITER_CHOCOLATE);
-    this->textctrl->StyleSetForeground (wxSTC_C_PREPROCESSOR,      CODEHIGHLITER_CRIMSON);
-    this->textctrl->StyleSetForeground (wxSTC_C_IDENTIFIER,        CODEHIGHLITER_ANTIQUE_WHITE);
-    this->textctrl->StyleSetForeground (wxSTC_C_NUMBER,            CODEHIGHLITER_CYAN);
-    this->textctrl->StyleSetForeground (wxSTC_C_CHARACTER,         CODEHIGHLITER_BEIGE);
-    this->textctrl->StyleSetForeground (wxSTC_C_WORD,              CODEHIGHLITER_VIOLET);
-    this->textctrl->StyleSetForeground (wxSTC_C_OPERATOR,          wxColor("WHITE"));
-    this->textctrl->StyleSetForeground (wxSTC_C_WORD2,             CODEHIGHLITER_CADET_BLUE);
-    this->textctrl->StyleSetForeground (wxSTC_C_COMMENT,           CODEHIGHLITER_CORN_SILK);
-    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTLINE,       CODEHIGHLITER_CORN_SILK);
-    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOC,        CODEHIGHLITER_CORN_SILK);
-    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOCKEYWORD, CODEHIGHLITER_BLUE_VIOLET);
+    this->textctrl->StyleSetForeground (wxSTC_C_DEFAULT,                this->textctrl->GetDefaultStyle().GetTextColour());
+    this->textctrl->StyleSetForeground (wxSTC_C_STRING,                 CODEHIGHLITER_CHOCOLATE);
+    this->textctrl->StyleSetForeground (wxSTC_C_PREPROCESSOR,           CODEHIGHLITER_CRIMSON);
+    this->textctrl->StyleSetForeground (wxSTC_C_IDENTIFIER,             CODEHIGHLITER_ANTIQUE_WHITE);
+    this->textctrl->StyleSetForeground (wxSTC_C_NUMBER,                 CODEHIGHLITER_CYAN);
+    this->textctrl->StyleSetForeground (wxSTC_C_CHARACTER,              CODEHIGHLITER_BEIGE);
+    this->textctrl->StyleSetForeground (wxSTC_C_WORD,                   CODEHIGHLITER_VIOLET);
+    this->textctrl->StyleSetForeground (wxSTC_C_OPERATOR,               wxColor("WHITE"));
+    this->textctrl->StyleSetForeground (wxSTC_C_WORD2,                  CODEHIGHLITER_CADET_BLUE);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENT,                CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTLINE,            CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOC,             CODEHIGHLITER_CORN_SILK);
+    this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOCKEYWORD,      CODEHIGHLITER_BLUE_VIOLET);
     this->textctrl->StyleSetForeground (wxSTC_C_COMMENTDOCKEYWORDERROR, CODEHIGHLITER_RED);
-    this->textctrl->StyleSetForeground (wxSTC_C_USERLITERAL,       CODEHIGHLITER_DARK_BLUE);
+    this->textctrl->StyleSetForeground (wxSTC_C_USERLITERAL,            CODEHIGHLITER_DARK_BLUE);
 
-    // sample list of keywords, I haven't included them all to keep it short...
+    // sample list of keywords
     this->textctrl->SetKeyWords(0, wxT("return for while break continue"));
     this->textctrl->SetKeyWords(1, wxT("const int float void char double"));
     this->textctrl->SetKeyWords(2, wxT("class struct std"));
 
-    return true;
 }
 
-bool CodeHighliter::highlightPython()
+void CodeHighliter::highlightPython()
 {
     this->lex_language=wxSTC_LEX_PYTHON;
 
@@ -60,22 +59,22 @@ bool CodeHighliter::highlightPython()
 
 }
 
-bool CodeHighliter::highlightJava()
+void CodeHighliter::highlightJava()
 {
 
 }
 
-bool CodeHighliter::highlightC()
+void CodeHighliter::highlightC()
 {
 
 }
 
-bool CodeHighliter::highlightJavaScript()
+void CodeHighliter::highlightJavaScript()
 {
 
 }
 
-bool CodeHighliter::highlightCSharp()
+void CodeHighliter::highlightCSharp()
 {
 
 }
